2c6651479c8473c58cf33a577cf366bbe6ad0a8b
BrianBellamy <brian.bellamy3@gmail.com>
2022-05-04 12:46:15 +0300

    my commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { CssBaseline } from '@mui/material';
+
+const App = () => {
+  return (
+    <div>
+      <CssBaseline>
+        <h1> my diplom </h1>
+      </CssBaseline>
+    </div>
+  );
+}
+
+export default App;
