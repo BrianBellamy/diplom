b09f44b947749adc2a471b5c8c981ad541c2afd1
BrianBellamy <brian.bellamy3@gmail.com>
2022-07-30 20:18:41 +0300

    последнее обновление

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
-import { Box, TextField, Button, Typography, List, ListItem, ListItemText, IconButton} from '@mui/material'
+import { Box, TextField, Button, List, ListItem, ListItemText, IconButton, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Typography} from '@mui/material'
 import React, {useState} from 'react'
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import DeleteIcon from '@mui/icons-material/Delete';
 import '../app.css'
+import Checkbox from '@mui/material/Checkbox';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import { bgcolor } from '@mui/system';
 
 const Home = () => {
@@ -26,25 +29,46 @@ const addTodo = () =>{
   const tmpArr = todos
   tmpArr.push({
     title: newTodo,
-    complete: true
+    complete: false
   })
   setTodos([...tmpArr])
   setNewTodo('')
 }
 
 
-const deleteTodo = (index) =>{
+const ConfirmDeleteTodo = (index) => {
+    setIndexToDelete(index)
+    setShowDialog(true)
+}
+
+
+const deleteTodo = (index) => {
   const tmpArr = todos
-  tmpArr.splice(index, 1)
+  tmpArr.splice(indexToDelete, 1)
+  setTodos([...tmpArr])
+  setShowDialog(false)
+  setIndexToDelete(-1)
+}
+
+
+const toggleTodo = (index) =>{
+  const tmpArr = todos
+  tmpArr[index].complete=!tmpArr[index].complete
   setTodos([...tmpArr])
 }
-const toggleTodo = (index) =>{}
 
+const closeDialog = () => {
+  setShowDialog(false)
+}
+
+
+const [showDialog, setShowDialog] = useState(false)
 const [newTodo, setNewTodo] = useState("")
+const [indexToDelete, setIndexToDelete]= useState(-1)
 
 
   return (
-    <Box>
+    <React.Fragment>
         <Box sx={{
     boxShadow: '2',
     padding: 3
@@ -83,14 +107,25 @@ const [newTodo, setNewTodo] = useState("")
       mt:2,
       boxShadow: '2',
     }}>
-    {todos.length > 0 && 
+    {todos.length > 0 &&
     todos.map((todo, index) => {
       return(
         <ListItem key={index}
         secondaryAction={
-          <IconButton onClick = {() => deleteTodo(index)}>
-            <DeleteIcon/>
-          </IconButton>
+          <React.Fragment>
+            <IconButton 
+            onClick = {() => toggleTodo(index)}
+            >
+              {
+                 todo.complete ?
+                 <CheckBoxIcon color='success'/>
+                 : <CheckBoxOutlineBlankIcon/>
+              }
+           </IconButton>
+            <IconButton onClick = {() => ConfirmDeleteTodo(index)}>
+              <DeleteIcon color='error'/>
+           </IconButton>
+          </React.Fragment>
         }>
           <ListItemText primary={todo.title}
           secondary={todo.complete ? 'complete': 'incomplete'}>
@@ -99,8 +134,38 @@ const [newTodo, setNewTodo] = useState("")
         </ListItem>
       )
     })}
+    {todos.length === 0 && (
+      <ListItem>
+        <ListItemText primary='Not todos yet...'>
+
+        </ListItemText>
+      </ListItem>
+    )}
       </List>
-    </Box>
+
+        <Dialog 
+        open={showDialog}
+        onClose={closeDialog}
+        >
+          <DialogTitle>
+            COnfir deliting
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText component='div'>
+              Are your sure you want to delete?
+              <Typography variant='h5' component='p'>
+                  {indexToDelete >= 0 && todos[indexToDelete].title}
+              </Typography>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button variant='contained' color='error' onClick={deleteTodo}> Yes</Button>
+            <Button variant='contained' color='success' onClick={closeDialog}>No</Button>
+          </DialogActions>
+          
+        </Dialog>
+
+    </React.Fragment>
     
     
   )
